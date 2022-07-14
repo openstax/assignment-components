aaacbc6468de652ba033108ee3d9c9e168115f2b
Josiah Ivey <josiah.ivey@gmail.com>
2022-07-14 11:26:43 -0700

    add basic Answer story

---
+++ src/components/Answer.stories.tsx
@@ -0,0 +1,25 @@
+import { Answer, AnswerProps } from './Answer';
+const props: AnswerProps = {
+  type: 'student',
+  iter: 0,
+  answer: {
+    id: '1',
+    correctness: null,
+    isCorrect: true,
+    content_html: 'Anwer',
+    selected_count: 5
+  },
+  onChangeAnswer: () => {},
+  disabled: false,
+  onKeyPress: () => {},
+  qid: 1,
+  hasCorrectAnswer: false,
+  chosenAnswer: [],
+  incorrectAnswerId: 0,
+  answered_count: 0,
+};
+
+export const Default = () => <Answer {...props} />;
+export const Checked = () => <Answer {...props} chosenAnswer={['1']} />;
+export const Correct = () => <Answer {...props} correctAnswerId='1' />;
+export const Incorrect = () => <Answer {...props} incorrectAnswerId='1' />;
