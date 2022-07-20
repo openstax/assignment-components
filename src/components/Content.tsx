dd7cd750c1a9e0619b31f503742626ba615cef34
Josiah Ivey <josiah.ivey@gmail.com>
2022-07-20 15:31:29 -0700

    add content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+type ComponentType = keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>;
+
+interface ContentProps<T extends ComponentType | undefined> {
+  className?: string;
+  component?: T extends undefined ? undefined :
+    T extends ComponentType ? React.ReactComponentElement<T>:
+    never;
+  html: string;
+  block?: boolean;
+}
+
+export const Content = (<T extends ComponentType | undefined>(
+  {html, component, ...props}: ContentProps<T>
+) => {
+  if (component !== undefined) {
+    return React.cloneElement(component, {html, ...props});
+  }
+  return <div dangerouslySetInnerHTML={{ __html: html }} {...props} />;
+});
